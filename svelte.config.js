5f2761dbb32f443aa560ac1a6408eb8ea3eefbf5
Sirko Schindler <git@sirko.org>
2021-10-28 16:08:38 +0200

    prerender fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import * as Glob from 'glob-promise';
+
+// list of all files to be generated for the data
+const files = Glob.default.sync( '*.yaml', { cwd: 'data'} );
+const dataFiles = files.flatMap( (f) => {
+  f = f.replace( /.yaml$/, '' );
+  return [
+    `/data/${f}`,
+    `/data/${f}.ttl`,
+    `/data/${f}.json`,
+  ];
+});
+const mdFiles = Glob.default
+  .sync( '*.md', { cwd: 'texts'} )
+  .map( (f) => `/md/${f}` );
+console.log( mdFiles );
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,6 +30,11 @@ const config = {
       onError: 'continue',
       entries: [
         '*',
+        '/data/summary.json',
+        '/data/envthes',
+        '/data/envthes.json',
+        ... dataFiles,
+        ... mdFiles,
       ],
     },
   },
